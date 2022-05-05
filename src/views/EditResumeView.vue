9075682762d159ca0e55ef0bab3a8c8c8b11af76
hollymsnell <hollymsnell@gmail.com>
2022-05-04 18:42:52 -0600

    added template on editresume

--- src/views/EditResumeView.vue
+++ src/views/EditResumeView.vue
@@ -28,3 +28,50 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>Edit Resume</h1>
+    <form v-on:submit.prevent="editResume()">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error" style="color: red">
+          {{ error }}
+        </li>
+      </ul>
+      <p>
+        Experience Info:
+        <input type="text" v-model="student.experience.job_title" />
+      </p>
+      <p>
+        <input type="text" v-model="student.experience.start_date" />
+      </p>
+      <p>
+        <input type="text" v-model="student.experience.end_date" />
+      </p>
+      <p>
+        <input type="text" v-model="student.experience.details" />
+      </p>
+      <p>
+        <input type="text" v-model="student.skills.skill_name" />
+      </p>
+      <p>
+        Education Info:
+        <input type="text" v-model="student.education.degree" />
+      </p>
+
+      <p>
+        <input type="text" v-model="student.education.university_name" />
+      </p>
+      <p>
+        <input type="text" v-model="student.education.details" />
+      </p>
+      <p>
+        <input type="text" v-model="student.education.start_date" />
+      </p>
+      <p>
+        <input type="text" v-model="student.education.end_date" />
+      </p>
+      <input type="submit" value="Edit" />
+    </form>
+  </div>
+</template>
